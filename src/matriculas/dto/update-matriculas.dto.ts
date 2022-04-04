b7452fe038115303887d5e2796b5429a246045c1
Fergoes <fergoes09@gmail.com>
2022-04-04 10:47:41 -0300

    att

--- src/matriculas/dto/update-matriculas.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateMatriculasDto } from "./create-Matriculas.dto";
-
-export class UpdateMatriculasDto extends PartialType(CreateMatriculasDto) {}
